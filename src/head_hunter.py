8a7230aa3d4be0a9ae0797f232d1ec175f2e08d1
Maksim Kamenev <maksim-kamenev96@mail.ru>
2024-05-21 23:14:19 +0300

    update class HeadHunter

--- src/head_hunter.py
+++ src/head_hunter.py
@@ -1,50 +1,30 @@
 import json
-
+import os
+from pprint import pprint
+from config import ROOT_DIR
 import requests
 
 
-class HeadHunterEmployer:
-    """
-    Класс для работы с API сайта HeadHunter. Получение данных о
-    работодателе
-    """
-
-    def __init__(self):
-        self.url = 'https://api.hh.ru/employers'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'page': 0, 'text': '', 'locale': 'RU', 'per_page': 100,
-                       'only_with_salary': True}
-        self.employers = []
-
-    def load_employers(self, keyword: str):
-        """
-        Загружаем список работодателей для выбора 10 из них
-        :param keyword: Слово для поиска компании
-        """
-        self.params['text'] = keyword
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers,
-                                    params=self.params)
-            employers = response.json()['items']
-            self.employers.extend(employers)
-            self.params['page'] += 1
-        return self.employers
-
-
-class HeadHunterVacancy:
+class HeadHunterAPI:
     """Класс для работы с API сайта HeadHunter. Получения данных о вакансиях"""
 
-    def __init__(self, employer_id):
+    def __init__(self):
         """
-        Конструктор класса  HeadHunter
+        Конструктор класса  HeadHunterAPI
         """
-        self.url = 'https://api.hh.ru/vacancies'
+        self.url = ''
         self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = ''
+        self.vacancies = []
+        self.employers = []
+
+    def load_vacancies(self, employer_id) -> list:
+        """Метод для загрузки вакансий по ID работодателя"""
+
+        self.url = f'https://api.hh.ru/vacancies'
         self.params = {'page': 0, 'area': 113, 'currency': 'RUR',
                        'per_page': 100, 'employer_id': employer_id}
-        self.vacancies = []
 
-    def load_vacancies(self):
         while self.params.get('page') != 20:
             response = requests.get(self.url, headers=self.headers,
                                     params=self.params)
@@ -52,3 +32,37 @@ class HeadHunterVacancy:
             self.vacancies.extend(vacancies)
             self.params['page'] += 1
         return self.vacancies
+
+    @staticmethod
+    def default_employers_id() -> list:
+        """
+        Выбираем ID 10 компаний по умолчанию и возвращаем список.
+        :return: Список ID работодателей выбранные по умолчанию из файла
+        """
+        path = os.path.join(ROOT_DIR, 'data', 'default_id_employers.json')
+
+        list_ids_employers = []
+        try:
+            with open(path, 'r', encoding='utf-8') as file:
+                for employer_id in json.load(file):
+                    for id_emp in employer_id.values():
+                        list_ids_employers.append(id_emp)
+                return list_ids_employers
+        except FileNotFoundError:
+            print('Файл не найден')
+
+    def load_employers(self, list_ids_employers) -> list:
+        """Метод для загрузки информации о работодателе по ID"""
+        for employers_id in list_ids_employers:
+            self.url = f'https://api.hh.ru/employers/{employers_id}'
+            self.params = {'locale': 'RU', 'host': 'hh.ru'}
+            response = requests.get(self.url, headers=self.headers,
+                                    params=self.params)
+            employer = response.json()
+            self.employers.extend(employer)
+            return self.employers
+
+
+if __name__ == "__main__":
+    list_emp1 = HeadHunterAPI().load_vacancies(1740)[:2]
+    pprint(list_emp1[0])
